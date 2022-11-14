139df675d34e93ef7d4b3f2c1fdb93d6f9b34dd4
adriwco <adriwco@gmail.com>
2022-11-14 17:09:46 -0300

    funções: finalizar, editar, cancelar e deletar

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+let oldInputValue;
+
 // Funções
 const saveTodo = (text) => {
   const todo = document.createElement("div");
@@ -33,6 +35,23 @@ const saveTodo = (text) => {
   todoInput.value = "";
   todoInput.focus();
 } 
+
+const toggleForms = () => {
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+}
+
+const updateTodo = (editInputValue) => {
+  const todos = document.querySelectorAll(".todo");
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+    if(todoTitle.innerHTML === oldInputValue){
+      todoTitle.innerHTML = editInputValue;
+    }
+  });
+}
+
 // Eventos
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -44,8 +63,42 @@ todoForm.addEventListener("submit", (e) => {
 
 document.addEventListener("click", (e) => {
   const targetEl = e.target;
+  const parentEl = targetEl.closest("div");
+  let todoTitle;
+
+  if(parentEl && parentEl.querySelector("h3")){
+    todoTitle = parentEl.querySelector("h3").innerHTML;
+  }
+
   if(targetEl.classList.contains("finish-todo")){
     console.log('ok')
-    // https://youtu.be/HSssE1PRQcA?t=2498
+    parentEl.classList.toggle("done");
+  }
+
+  if(targetEl.classList.contains("remove-todo")){
+    parentEl.remove();
+  }
+
+  if(targetEl.classList.contains("edit-todo")){
+    toggleForms();
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
   }
 })
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleForms();
+})
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const editInputValue = editInput.value;
+  if(editInputValue){
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+})
+
+// Proxio passo: busca, filtro e gerenciar pela local storage
