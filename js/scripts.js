de7e8b11ebc39e7fbf4a022fa48cedb6a17c898b
adriwco <adriwco@gmail.com>
2023-03-21 18:57:28 -0300

    add > editInput > focus() & select()

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const inputSearch = document.querySelector("#site-search");
-const contadorMsg = document.querySelector('#contador');
+const contadorMsg = document.querySelector("#contador");
 let oldInputValue;
 
 // Funções
@@ -17,7 +17,7 @@ const saveTodo = (text) => {
   const todoTitle = document.createElement("h3");
   todoTitle.innerHTML = text;
   todo.appendChild(todoTitle);
-  
+
   const doneBtn = document.createElement("button");
   doneBtn.classList.add("finish-todo");
   doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
@@ -36,62 +36,64 @@ const saveTodo = (text) => {
   todoList.appendChild(todo);
   todoInput.value = "";
   todoInput.focus();
-}
+};
 
 const toggleForms = () => {
   editForm.classList.toggle("hide");
   todoForm.classList.toggle("hide");
   todoList.classList.toggle("hide");
-}
+};
 
 const updateTodo = (editInputValue) => {
   const todos = document.querySelectorAll(".todo");
   todos.forEach((todo) => {
     let todoTitle = todo.querySelector("h3");
-    if(todoTitle.innerHTML === oldInputValue){
+    if (todoTitle.innerHTML === oldInputValue) {
       todoTitle.innerHTML = editInputValue;
     }
   });
-}
+};
 
-const filterTodos = (todos, inputValueSearch, returnMatchedTodos) => todos
-  .filter(todo => {
-    const matchedTodos = todo.textContent.toLowerCase().includes(inputValueSearch)
-    return returnMatchedTodos ? matchedTodos : !matchedTodos
-  })
+const filterTodos = (todos, inputValueSearch, returnMatchedTodos) =>
+  todos.filter((todo) => {
+    const matchedTodos = todo.textContent
+      .toLowerCase()
+      .includes(inputValueSearch);
+    return returnMatchedTodos ? matchedTodos : !matchedTodos;
+  });
 const manipulateClasses = (todo, classToAdd, classToRemove) => {
-  todo.forEach(todo => {
-    todo.classList.add(classToAdd)
-    todo.classList.remove(classToRemove)
-  })
-}
+  todo.forEach((todo) => {
+    todo.classList.add(classToAdd);
+    todo.classList.remove(classToRemove);
+  });
+};
 const hideTodos = (todos, inputValueSearch) => {
-  const todosToHide = filterTodos(todos, inputValueSearch, false)
-  manipulateClasses(todosToHide, 'hide', null)
- }
- const showTodos = (todos, inputValueSearch) => {
-  const todosToShow = filterTodos(todos, inputValueSearch, true)
-  manipulateClasses(todosToShow, null, 'hide')
- }
+  const todosToHide = filterTodos(todos, inputValueSearch, false);
+  manipulateClasses(todosToHide, "hide", null);
+};
+const showTodos = (todos, inputValueSearch) => {
+  const todosToShow = filterTodos(todos, inputValueSearch, true);
+  manipulateClasses(todosToShow, null, "hide");
+};
 
-  function mudarCorContador(numero){
-    if(numero >= 11 && numero <= 32){
-      contadorMsg.style.color = "#999900";
-    }else if(numero >= 33 && numero <= 58){
-      contadorMsg.style.color = "#CC6600";
-    }else if(numero === 59){
-      contadorMsg.style.color = "#FF0000";
-    }else{
-      contadorMsg.style.color = "#000";
-    }
+function mudarCorContador(numero) {
+  if (numero >= 11 && numero <= 32) {
+    contadorMsg.style.color = "#999900";
+  } else if (numero >= 33 && numero <= 58) {
+    contadorMsg.style.color = "#CC6600";
+  } else if (numero === 59) {
+    contadorMsg.style.color = "#FF0000";
+  } else {
+    contadorMsg.style.color = "#000";
   }
+}
 
 // JQuery 3.5.1
-$(document).ready(function(){
-  $('input').bind('cut copy paste',function(e) {
-      e.preventDefault();
+$(document).ready(function () {
+  $("input").bind("cut copy paste", function (e) {
+    e.preventDefault();
   });
-  $('input').on('keypress', function(e) {
+  $("input").on("keypress", function (e) {
     if (e.which === 32 && $(this).val().length === 0) {
       e.preventDefault();
     }
@@ -108,55 +110,57 @@ $(document).on("input", "#todo-input", function () {
 // Eventos
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const inputValue = todoInput.value
-  if(inputValue){
+  const inputValue = todoInput.value;
+  if (inputValue) {
     saveTodo(inputValue);
   }
-})
+});
 
 document.addEventListener("click", (e) => {
   const targetEl = e.target;
   const parentEl = targetEl.closest("div");
   let todoTitle;
 
-  if(parentEl && parentEl.querySelector("h3")){
+  if (parentEl && parentEl.querySelector("h3")) {
     todoTitle = parentEl.querySelector("h3").innerHTML;
   }
 
-  if(targetEl.classList.contains("finish-todo")){
+  if (targetEl.classList.contains("finish-todo")) {
     parentEl.classList.toggle("done");
   }
 
-  if(targetEl.classList.contains("remove-todo")){
+  if (targetEl.classList.contains("remove-todo")) {
     parentEl.remove();
   }
 
-  if(targetEl.classList.contains("edit-todo")){
+  if (targetEl.classList.contains("edit-todo")) {
     toggleForms();
     editInput.value = todoTitle;
     oldInputValue = todoTitle;
+    editInput.focus();
+    editInput.select();
   }
-})
+});
 
 cancelEditBtn.addEventListener("click", (e) => {
   e.preventDefault();
   toggleForms();
-})
+});
 
 editForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const editInputValue = editInput.value;
-  if(editInputValue){
+  if (editInputValue) {
     updateTodo(editInputValue);
   }
   toggleForms();
-})
+});
 
-inputSearch.addEventListener("input", event => {
-  const inputValueSearch = event.target.value.trim().toLowerCase()
-  const todos = filterH3 = Array.from(todoList.children)
-  hideTodos(todos, inputValueSearch)
-  showTodos(todos, inputValueSearch)
-})
+inputSearch.addEventListener("input", (event) => {
+  const inputValueSearch = event.target.value.trim().toLowerCase();
+  const todos = (filterH3 = Array.from(todoList.children));
+  hideTodos(todos, inputValueSearch);
+  showTodos(todos, inputValueSearch);
+});
 
- // Proxio passo: filtro e gerenciar pela local storage 
+// Proxio passo: filtro e gerenciar pela local storage
